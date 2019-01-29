7ce0d63151da4ea910a1982026319b7b0aab7171
Mike <leemikeholland@gmail.com>
2019-01-29 17:42:51 +0700

    basic framework

--- header.php
+++ header.php
@@ -4,3 +4,16 @@
       <?php wp_head();?>
     </head>
 <body <?php body_class();?>>
+
+<header class="sticky-top">
+
+  <dic class="container">
+    <?php wp_nav_menu (
+      array(
+        "theme_location" => "top-menu",
+        "menu_class" => "navigation",
+      )
+    );?>
+  </div>
+
+</header>
